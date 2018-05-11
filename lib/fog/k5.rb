31935e672e1d69886834559d36a2d24b0041ee99
blooper05 <legend.of.blooper@gmail.com>
2018-05-11 14:41:21 +0900

    :shell: bundle gem . --coc --mit '--test=rspec'

---
+++ lib/fog/k5.rb
@@ -0,0 +1,7 @@
+require "fog/k5/version"
+
+module Fog
+  module K5
+    # Your code goes here...
+  end
+end
---
+++ spec/fog/k5_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Fog::K5 do
+  it "has a version number" do
+    expect(Fog::K5::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
