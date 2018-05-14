21d3b22b013dae8aa7d6bb56bb092bd9a14b9c15
blooper05 <legend.of.blooper@gmail.com>
2018-05-14 21:41:07 +0900

    :+1: Add files method to storage directory

--- lib/fog/storage/k5/models/directory.rb
+++ lib/fog/storage/k5/models/directory.rb
@@ -2,6 +2,14 @@ module Fog
   module Storage
     class K5
       class Directory < Fog::Model
+        identity :key
+
+        def files
+          @files ||= Fog::Storage::K5::Files.new(
+            directory: self,
+            service:   service,
+          )
+        end
       end
     end
   end
