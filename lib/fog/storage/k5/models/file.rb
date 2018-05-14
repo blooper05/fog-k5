96d1e7879a73bb809366df412d8bbc9f1cc5671d
blooper05 <legend.of.blooper@gmail.com>
2018-05-14 21:42:57 +0900

    :+1: Add public_url method to storage file

--- lib/fog/storage/k5/models/file.rb
+++ lib/fog/storage/k5/models/file.rb
@@ -2,12 +2,25 @@ module Fog
   module Storage
     class K5
       class File < Fog::Model
+        include Fog::K5::Shared
+
         identity :key
 
         attribute :body
         attribute :content_length, type: :integer
         attribute :content_type
         attribute :directory
+
+        def public_url
+          requires :directory, :key
+
+          project_id = Fog.credentials[:k5_project_id]
+          version    = K5_STORAGE_URL_VERSION
+          url        = build_url(url_type: K5_STORAGE_URL_TYPE)
+          container  = directory.key
+
+          "#{url}/#{version}/AUTH_#{project_id}/#{container}/#{key}"
+        end
 
         def save
           requires :body, :directory, :key
