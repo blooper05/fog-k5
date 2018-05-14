126f2d602f4fd061c085dfeaf4687b4041ee04ef
blooper05 <legend.of.blooper@gmail.com>
2018-05-14 21:42:15 +0900

    :+1: Add save method to storage file

--- lib/fog/storage/k5/models/file.rb
+++ lib/fog/storage/k5/models/file.rb
@@ -2,6 +2,23 @@ module Fog
   module Storage
     class K5
       class File < Fog::Model
+        identity :key
+
+        attribute :body
+        attribute :content_length, type: :integer
+        attribute :content_type
+        attribute :directory
+
+        def save
+          requires :body, :directory, :key
+
+          service.put_object(directory.key, key, body)
+
+          self.content_length = Fog::Storage.get_body_size(body)
+          self.content_type ||= Fog::Storage.get_cotent_type(body)
+
+          true
+        end
       end
     end
   end
