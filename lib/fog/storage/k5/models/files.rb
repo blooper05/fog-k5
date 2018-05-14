9e07dacfe2ee32da64d225db7d53d99bb5f07681
blooper05 <legend.of.blooper@gmail.com>
2018-05-14 21:41:29 +0900

    :+1: Add new method to storage files

--- lib/fog/storage/k5/models/files.rb
+++ lib/fog/storage/k5/models/files.rb
@@ -3,6 +3,13 @@ module Fog
     class K5
       class Files < Fog::Collection
         model Fog::Storage::K5::File
+
+        attribute :directory
+
+        def new(attributes = {})
+          requires :directory
+          super({ directory: directory }.merge(attributes))
+        end
       end
     end
   end
